1ec794a93841ddc50102f47dd7088a9f7c2352b1
Linkiline <39643537+Linkiline@users.noreply.github.com>
2018-05-26 07:24:57 +0000

    удаление основного кода
    удаление основного кода

--- src/test/java/testfor/testNG.java
+++ src/test/java/testfor/testNG.java
@@ -11,29 +11,8 @@ public class testNG {
                 // Подключаем драйвер браузера
                 setProperty("webdriver.chrome.driver","D:\\Project\\chromedriver\\chromedriver.exe");
 
-                // Открываем браузер, разворачиваем и переходим на сайт
-                WebDriver driver = new ChromeDriver();
-                driver.manage().window().maximize();
-                driver.get("http://igorakintev.ru/admin/");
-
-                // По ID ищем поля User и Password, с помощью метода sendKeys вводим данные
-                driver.findElement(By.id("id_username")).sendKeys("silenium");
-                driver.findElement(By.id("id_password")).sendKeys("super_password");
-
-                // С помощью локатора css ищем класс кнопки и нажимаем
-                driver.findElement(By.cssSelector("div.submit-row")).click();
-
-                // С помощью локатора xpath ищем элемент с  ID=content и идем к следующему элементу
-                // Получаем текст и сравниваем
-                String namePanel = driver.findElement(By.xpath("//*[@id='content']/h1")).getText();
-                Assert.assertEquals("Панель управления", namePanel);
-
-            }
-        //изменения от пользюкааа !!!!!!!!!!!!
-    //изменения от пользюкааа !!!!!!!!!!!!
-    //изменения от пользюкааа !!!!!!!!!!!!
-    //изменения от пользюкааа !!!!!!!!!!!!
-    //изменения от пользюкааа !!!!!!!!!!!!
+                       }
+   
            
         }
 
